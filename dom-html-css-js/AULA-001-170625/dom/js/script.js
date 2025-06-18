7d478f356ae886637701788234ceefed6b4ffe27
Thiago S. Rodrigues <tsr.souza@gmail.com>
2025-06-17 22:22:17 -0300

    Subindo nova estrutura.

---
+++ dom-html-css-js/AULA-001-170625/dom/js/script.js
@@ -0,0 +1,11 @@
+function show() {
+    const contactList = document.getElementById('contact-list');
+    console.log(contactList);
+    
+    const liElements = document.getElementsByTagName('li');
+    console.log(liElements);
+    
+    const contactInputs = document.getElementsByClassName('contact-input')
+    console.log(contactInputs);
+    
+}
---
+++ dom-html-css-js/AULA-001-170625/js/script.js
@@ -0,0 +1,15 @@
+function mouseEnterEvent() {
+    console.log("O usuário passou o cursor sobre o input!");
+}
+
+function onChangeEvent() {
+    console.log("O valor do input foi alterado.");
+}
+
+function onFocusEvent() {
+    console.log("O input foi selecionado (foco).");
+}
+
+function onBlurEvent() {
+    console.log("O input perdeu o foco.");
+}
